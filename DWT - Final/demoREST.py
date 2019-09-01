e8d6d3d4dd0ce7a81240362d6fa711b0a265effb
Anubhav Gupta <50870959+anubhav25gupta@users.noreply.github.com>
2019-09-01 21:08:55 +0200

    Update demoREST.py

--- DWT - Final/demoREST.py
+++ DWT - Final/demoREST.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from json import dumps
 import os
 #anubhav
-#paurush
 db_connect = create_engine('sqlite:///tutorial.db')
 app = Flask(__name__)
 api = Api(app)
